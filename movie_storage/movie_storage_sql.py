747a37529532846e7ae3b146708c278c80a7f4a3
jasonbdt <ja.bladt@gmail.com>
2025-11-10 11:23:57 +0100

    Move table creation into setup at function call

--- movie_storage/movie_storage_sql.py
+++ movie_storage/movie_storage_sql.py
@@ -20,33 +20,6 @@ DB_URL = "sqlite:///data/movies.db"
 # Create the engine
 engine = create_engine(DB_URL, echo=False)
 
-# Create the `movies` and `users` table if they does not exist
-with engine.connect() as connection:
-    connection.execute(text("""
-        CREATE TABLE IF NOT EXISTS users (
-            'id' INTEGER NOT NULL,
-            'name' TEXT NOT NULL,
-            PRIMARY KEY('id' AUTOINCREMENT)
-        );
-    """))
-
-    connection.execute(text("""
-        CREATE TABLE IF NOT EXISTS movies (
-            'id' INTEGER NOT NULL,
-            'user_id' INTEGER NOT NULL,
-            'imdb_id' TEXT NOT NULL,
-            'title' TEXT NOT NULL,
-            'year' INTEGER NOT NULL,
-            'rating' REAL NOT NULL,
-            'poster' TEXT NOT NULL,
-            'note' TEXT,
-            'country_iso2' TEXT NOT NULL,
-            PRIMARY KEY('id' AUTOINCREMENT),
-            FOREIGN KEY('user_id') REFERENCES 'users'('id')
-        );
-    """))
-    connection.commit()
-
 
 def list_movies() -> list[tuple[str, dict[str, Any]]]:
     """
